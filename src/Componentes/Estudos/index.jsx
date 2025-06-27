e2f9b427a689214de3e32cc80f793fee9857e918
Ivan.dias <mangakaivan@gmail.com>
2025-06-27 19:57:23 -0300

    add cv

--- src/Componentes/Estudos/index.jsx
+++ src/Componentes/Estudos/index.jsx
@@ -1,15 +1,19 @@
 import Style from './index.module.css';
-import { Desktop, Robot, Graph } from "@phosphor-icons/react";
+
 function Sobre() {
-    return (
-
-        <section className={Style.card}>
-
-            <iframe src="https://garrulous-mulberry-e65.notion.site/ebd/9c455ad581ee4b6295f7ff8e59b00751" width="100%" height="600" frameborder="0" allowfullscreen />
-
-        </section>
-
-    );
+  return (
+    <section className={Style.card}>
+      <iframe
+        src="https://garrulous-mulberry-e65.notion.site/ebd/9c455ad581ee4b6295f7ff8e59b00751"
+        width="100%"
+        height="600"
+        frameBorder="0"
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer"
+      />
+    </section>
+  );
 }
 
 export default Sobre;
